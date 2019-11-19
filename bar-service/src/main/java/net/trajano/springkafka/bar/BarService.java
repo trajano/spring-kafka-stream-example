b693de57f35e770b54ed945499eb94ad537e2f4a
Archimedes Trajano <archimedes.trajano@devhaus.com>
2019-11-19 11:01:10 -0500

    Multiple bars

--- bar-service/src/main/java/net/trajano/springkafka/bar/BarService.java
+++ bar-service/src/main/java/net/trajano/springkafka/bar/BarService.java
@@ -3,6 +3,7 @@ package net.trajano.springkafka.bar;
 import lombok.extern.slf4j.Slf4j;
 import net.trajano.springkafka.model.ReverseRequest;
 import net.trajano.springkafka.model.ReverseResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
@@ -14,11 +15,21 @@ import org.springframework.messaging.handler.annotation.SendTo;
 @Slf4j
 public class BarService {
 
+    @Value("${sleep:0}")
+    private long sleepInMillis;
+
+    @Value("${spring.application.name}")
+    private String source;
+
     @KafkaListener(topics = "reverse")
     @SendTo
-    public ReverseResponse reverse(ReverseRequest request) {
+    public ReverseResponse reverse(ReverseRequest request) throws InterruptedException {
         final String text = new StringBuilder(request.getText()).reverse().toString();
-        return new ReverseResponse(text);
+        if (sleepInMillis > 0) {
+            log.info("sleeping for {}ms", sleepInMillis);
+            Thread.sleep(sleepInMillis);
+        }
+        return new ReverseResponse(text, source);
     }
 
 
--- foo-service/src/main/java/net/trajano/springkafka/model/ReverseResponse.java
+++ foo-service/src/main/java/net/trajano/springkafka/model/ReverseResponse.java
@@ -9,4 +9,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ReverseResponse {
     private String text;
+    private String source;
 }
